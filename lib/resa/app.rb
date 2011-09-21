cceb66fad2cca573901d074901a147cc7082b7c1
Jean-Denis Vauguet <jd@vauguet.fr>
2011-09-21 22:42:22 +0200

    Add tests for the routes.
    Signed-off-by: Jean-Denis Vauguet <jdvauguet@af83.com>
    Signed-off-by: Shakaman <mbenadon@af83.com>

--- lib/resa/app.rb
+++ lib/resa/app.rb
@@ -7,11 +7,11 @@ module Resa
   class App < Sinatra::Base
     set :static, true
     set :public,  File.dirname(__FILE__) + '/../../public'
-    set :views, File.dirname(__FILE__) + '/../../views' 
+    set :views, File.dirname(__FILE__) + '/../../views'
 
     helpers do
       def find_room(id)
-        @room = Room.where(:name => id).first
+        @room = Room.where(:name => id).without(:_id).first
     rescue Mongoid::Errors::DocumentNotFound, BSON::InvalidObjectId
         halt 404, '404 - Page not found'
       end
@@ -33,21 +33,21 @@ module Resa
     end
 
     # Return room's availability for the current day
-    get '/room/:id' do
+    get '/rooms/:id' do
       find_room(params[:id])
       @room.to_json
     end
 
     # Return reservations of the day
-    get '/room/:id/reservations' do
+    get '/rooms/:id/reservations' do
       find_room(params[:id])
       reservations = @room.reservations_for_a_day
-      
+
       reservations.to_json
     end
 
     # Return reservations for a day
-    get '/room/:id/reservations/:year/:month/:day' do
+    get '/rooms/:id/reservations/:year/:month/:day' do
       find_room(params[:id])
       reservations = @room.reservations_for_a_day("#{params[:year]}-#{params[:month]}-#{params[:day]}")
 
@@ -57,7 +57,7 @@ module Resa
     # create new event
     post '/reservations' do
       data = JSON.parse(request.body.string)
-      
+
       if data.nil? then
         status 404
       else
@@ -70,7 +70,7 @@ module Resa
         rescue
           halt 404, '404 - Date error'
         end
-          
+
         # check if the room is available for this date
         @room.check_availability(data[:dtstart], data[:dtend])
 
--- lib/resa/calendar.rb
+++ lib/resa/calendar.rb
@@ -4,21 +4,21 @@ require 'open-uri'
 
 module Resa
   class Calendar
-    
+
     # Flush all events and re-import
-    def self.import
+    def self.import(filename = :reservations)
       server = Resa.config[:calendar][:server]
-      ics = Resa.config[:calendar][:test]
-      
+      ics = Resa.config[:calendar][filename]
+
       cal_file = open(server + ics).read
-      
+
       calendar = Icalendar.parse(cal_file)
       cal = calendar.first
       events = cal.events
 
       events.each do |e|
         room = Room.find_or_create(e.location).first
-        
+
         room.flush_events
 
         room.events.create(
@@ -36,7 +36,7 @@ module Resa
       ics = Resa.config[:calendar][:name]
 
       cal = Icalendar::Calendar.new
-      
+
       rooms = Resa::Room.all
       rooms.each do |room|
         events = room.events
